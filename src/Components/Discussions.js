fb0f27a71c6f204215e616c2a770922ddd297c18
es39 <esj10025@gmail.com>
2023-01-07 15:30:00 +0900

    Discussions.js modify

---
+++ src/Components/Discussions.js
@@ -0,0 +1,15 @@
+// TODO : useState를 react로 부터 import 합니다.
+import React, { useState } from 'react';
+import Footer from '../Footer';
+
+export const Discussions = (props) => {
+    return(
+        <section class="discussion__wrapper">
+            <ul class="discussions__container">
+                
+            </ul>
+        </section>
+    )
+};
+
+export default Discussions;
