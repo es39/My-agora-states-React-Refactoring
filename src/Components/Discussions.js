ba59bf33a31a633197862873c2ff1f208c45abb8
es39 <esj10025@gmail.com>
2023-01-07 17:43:56 +0900

    Discussions.js styled-components modify

--- src/Components/Discussions.js
+++ src/Components/Discussions.js
@@ -1,17 +1,30 @@
 import styled from 'styled-components';
 import React, { useState } from 'react';
 import '../index.css';
-
 import { Discussion } from './Discussion';
+
+const DiscussionsContainer = styled.div `
+    list-style-type: none;
+    background-color: white;
+    overflow: auto;
+    height: 540px;
+    border-radius: 20px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    box-shadow: 0px 0px 5px 1px rgba(0, 0, 0, 0.4);
+    &::-webkit-scrollbar{
+        display: none;
+    }
+`
 
 export const Discussions = ({ discussions }) => {
     return(
         <section className="discussion__wrapper">
-            <ul className="discussions__container">
+            <DiscussionsContainer>
                 {discussions.map(discussion => {
                  return <Discussion discussion={discussion} key={discussion.id}></Discussion>
                 })}
-            </ul>
+            </DiscussionsContainer>
         </section>
     );
 }
