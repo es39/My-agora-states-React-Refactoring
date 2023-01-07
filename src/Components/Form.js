ec1027436a4860b0f7d8fbef9a46494408e0a6cd
es39 <esj10025@gmail.com>
2023-01-07 17:43:29 +0900

    Form.js styled-components modify

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -5,8 +5,10 @@ const SubmitButton = styled.button`
     background-color: white;
     padding: 5px;
     margin: 10px;
+    border-radius: 10px;
     &:hover{
         cursor: pointer;
+        background-color: #f8f8f8;
     }
 `;
 
@@ -14,6 +16,14 @@ const FormContainer = styled.form `
     display: flex;
     flex-direction: column;
     padding: 10px 30px;
+`
+
+const FormInputWrapper = styled.div `
+    display: flex;
+    height: 150px;
+    width: 500px;
+    flex-direction: column;
+    justify-content: space-around;
 `
 
 const handleSubmit = (event) => {
@@ -25,7 +35,7 @@ export const Form = () => {
         <section className="form__container">
             <img src={require("../Image/agora-logo.png")} alt="아고라 숲 로고"/>
                 <FormContainer action="" method="get" className="form">
-                    <div className="form__input--wrapper">
+                    <FormInputWrapper className="form__input--wrapper">
                     <div className="form__input--name">
                         <input type="text" name="name" id="name" placeholder="  이름을 작성해줘 구리!" required="" />
                     </div>
@@ -35,9 +45,9 @@ export const Form = () => {
                     <div className="form__textbox">
                         <textarea id="story" name="story" placeholder="질문을 작성해줘 구리!" required=""></textarea>
                     </div>
-                    </div>
+                    </FormInputWrapper>
                     <div className="form__submit">
-                        <SubmitButton onClick={handleSubmit} type="submit" className="submit">제출해줘 구리!</SubmitButton>
+                        <SubmitButton onClick={handleSubmit} type="submit" className="form__submit">제출해줘 구리!</SubmitButton>
                     </div>
                 </FormContainer>
         </section>
