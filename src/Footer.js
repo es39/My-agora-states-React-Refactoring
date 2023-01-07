1cff8e14579cffa5b67c0b1d942e84fe681b360d
es39 <esj10025@gmail.com>
2023-01-07 17:27:17 +0900

    Footer.js modify

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+
+const FooterStyle = styled.footer`
+  text-align: right;
+  background-color: white;
+  padding: 7px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 5px 1px rgba(0, 0, 0, 0.4);
+  letter-spacing: 0.005px;
+`
 
 export const Footer = () => {
     return (
-      <footer>
+      <FooterStyle>
         2023 SunnyDay-AgoraStates
-      </footer>
+      </FooterStyle>
     );
   };
 
