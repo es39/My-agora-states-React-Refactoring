706fa4876140e6a7e998f9ad9a5558adad65f2e7
es39 <esj10025@gmail.com>
2023-01-07 17:25:27 +0900

    Form modify

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,15 +1,30 @@
 import styled from "styled-components";
+import './Form.css';
 
-const submitButton = styled.button`
-    background-color: yellow;
-    color: black;
+const SubmitButton = styled.button`
+    background-color: white;
+    padding: 5px;
+    margin: 10px;
+    &:hover{
+        cursor: pointer;
+    }
 `;
+
+const FormContainer = styled.form `
+    display: flex;
+    flex-direction: column;
+    padding: 10px 30px;
+`
+
+const handleSubmit = (event) => {
+    event.preventDefalut()
+}
 
 export const Form = () => {
     return (
         <section className="form__container">
-            <img src="../Image/agora-logo.png" alt="아고라 숲 로고"/>
-                <form action="" method="get" className="form">
+            <img src={require("../Image/agora-logo.png")} alt="아고라 숲 로고"/>
+                <FormContainer action="" method="get" className="form">
                     <div className="form__input--wrapper">
                     <div className="form__input--name">
                         <input type="text" name="name" id="name" placeholder="  이름을 작성해줘 구리!" required="" />
@@ -22,9 +37,9 @@ export const Form = () => {
                     </div>
                     </div>
                     <div className="form__submit">
-                        <button type="submit" className="submit">제출해줘 구리!</button>
+                        <SubmitButton onClick={handleSubmit} type="submit" className="submit">제출해줘 구리!</SubmitButton>
                     </div>
-                </form>
+                </FormContainer>
         </section>
     )
 }
