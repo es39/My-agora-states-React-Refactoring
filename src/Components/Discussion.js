c74e019324eb3c91ebe6d4464deebb34d6d9ce0b
es39 <esj10025@gmail.com>
2023-01-07 17:26:21 +0900

    Discussion.js modify

--- src/Components/Discussion.js
+++ src/Components/Discussion.js
@@ -1,20 +1,19 @@
+import styled from "styled-components"
+import '../index.css';
 
-
-export const Discussion = () => {
-    <li class="discussion__container">
-        <div class="discussion__avatar--wrapper">
-            <img class="discussion__avatar--image" src="https://avatars.githubusercontent.com/u/12145019?s=64&amp;u=5c97f25ee02d87898457e23c0e61b884241838e3&amp;v=4" alt="avatar of kimploo" />
-        </div>
-        <div class="discussion__content">
-            <h2 class="discussion__title">
-                <a href="https://github.com/codestates-seb/agora-states-fe/discussions/6">[notice] 좋은 질문하는 법</a>
-            </h2>
-            <div class="discussion__information">
-                kimploo / 2022-04-22T14:08:33Z
+export const Discussion = ({discussion}) => {
+    const {url, author, avatarUrl, title, createdAt, answer} = discussion;
+    return (
+        <li className="discussion__container">
+            <div className="discussion__avatar--wrapper">
+                <img className="discussion__avatar--image" src={avatarUrl} alt={`avatar of ${author}`} />
             </div>
-        </div>
-        <div class="discussion__answered"><p>✅</p></div>
-    </li>
-}
-
-export default Discussion
+            <div className="discussion__content">
+                <h2 className="discussion__title"><a href={url}>{title}</a></h2>
+                <div className="discussion__information">{`${author} / ${new Date(createdAt).toLocaleString()}`}</div>
+            </div>
+            <div className="discussion__answered">
+                <p className="discussion__isAnswered">{answer ? "✅" : "❎" }</p>
+            </div>
+        </li>
+    )}
