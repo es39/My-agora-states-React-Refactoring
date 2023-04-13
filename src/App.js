a10fe26bea470e5f63b0bd2ea30cb44d35ddb9aa
es39 <esj10025@gmail.com>
2023-04-13 19:34:01 +0900

    refactor : fatch code 주석

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
-import './App.css';
-import './index.css'
-import React from 'react';
-import { useState, useEffect } from 'react'
-
-import { Form }  from './Components/Form'
-import { Discussions } from './Components/Discussions'
-import Discussion from './Components/Discussion'
-import Footer from './Footer'
-import GlobalStyle from './GlobalStyle';
-
-
+import React, { useState } from "react";
+import "./App.css";
+import { Discussions } from "./Components/Discussions";
+import { Form } from "./Components/Form";
+import Footer from "./Footer";
+import GlobalStyle from "./GlobalStyle";
+import agoraStatesDiscussions from "./data/data";
+import "./index.css";
 
 function App() {
-  const [discussions, setDiscussions] = useState([]);
-  
-  useEffect(() => {
-    fetch("http://localhost:4000/discussions")
-    .then(res => res.json())
-    .then(data => {
-      setDiscussions(data)
-    }, [])
-  })
+  const [discussions, setDiscussions] = useState(agoraStatesDiscussions);
+  // useEffect(() => {
+  //   fetch("http://localhost:4000/discussions")
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     setDiscussions(data)
+  //   }, [])
+  // })
 
   return (
     <div>
-        <main>
-          <section>
-            <GlobalStyle />
-            <Form discussions={discussions} setDiscussions={setDiscussions}/>
-            <Discussions discussions={discussions}/>
-            </section>
-        </main>
-        <Footer />
+      <main>
+        <section>
+          <GlobalStyle />
+          <Form discussions={discussions} setDiscussions={setDiscussions} />
+          <Discussions
+            discussions={discussions}
+            setDiscussions={setDiscussions}
+          />
+        </section>
+      </main>
+      <Footer />
     </div>
   );
 }
